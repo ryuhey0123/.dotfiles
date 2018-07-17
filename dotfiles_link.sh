a6f7ccb1c853ee2f946e3e8696f1f574e52c257b
ryuhey_ojparch <ryuhei@ojparch.localdomain>
2018-07-17 17:52:21 +0900

    [Add]linux settings

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-ln -sf ~/.dotfiles/.vim ~/.vim
-ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+# zsh
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sh ~/.dotfiles/.latexmkrc ~/.latexmkrc
+
+# vim/neovim
+ln -sf ~/.dotfiles/.vim ~/.vim
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
+
+# LaTeX
+ln -sf ~/.dotfiles/.latexmkrc ~/.latexmkrc
+
+# i3
+ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
+ln -sf ~/.dotfiles/.Xresources ~/.Xresources
+ln -sf ~/.dotfiles/.xprofile ~/.xprofile
 
