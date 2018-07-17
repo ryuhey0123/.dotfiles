fa0faf254c9ab8615e76754a709d113de234d951
ryuhey_biz <vc.ryuhey@gmail.com>
2018-07-17 18:50:08 +0900

    [Fix] 余分なシンボリックリンクが作成されていた点を修正

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -7,12 +7,15 @@ ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.vim ~/.vim
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
+rm -rf ~/.dotfiles/.config/nvim/nvim
 
 # LaTeX
 ln -sf ~/.dotfiles/.latexmkrc ~/.latexmkrc
 
 # i3
 ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
+rm -rf ~/.dotfiles/.config/i3/i3
+
 ln -sf ~/.dotfiles/.Xresources ~/.Xresources
 ln -sf ~/.dotfiles/.xprofile ~/.xprofile
 
