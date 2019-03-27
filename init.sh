fe64d09478a29086e38ff9d991d06e08906e4766
ryuhey_biz <vc.ryuhey@gmail.com>
2019-03-27 17:32:39 +0900

    [update] init.sh

--- init.sh
+++ init.sh
@@ -1,36 +1,95 @@
 #!/bin/sh
+cat << 'EOS'
+
+           /$$             /$$      /$$$$$$  /$$ /$$                    
+          | $$            | $$     /$$__  $$|__/| $$                    
+      /$$$$$$$  /$$$$$$  /$$$$$$  | $$  \__/ /$$| $$  /$$$$$$   /$$$$$$$
+     /$$__  $$ /$$__  $$|_  $$_/  | $$$$    | $$| $$ /$$__  $$ /$$_____/
+    | $$  | $$| $$  \ $$  | $$    | $$_/    | $$| $$| $$$$$$$$|  $$$$$$ 
+    | $$  | $$| $$  | $$  | $$ /$$| $$      | $$| $$| $$_____/ \____  $$
+ /$$|  $$$$$$$|  $$$$$$/  |  $$$$/| $$      | $$| $$|  $$$$$$$ /$$$$$$$/
+|__/ \_______/ \______/    \___/  |__/      |__/|__/ \_______/|_______/ 
+                                                                        
+Welcome.
+This is Ryuhey's dotfiles.
+
+EOS
 
 # zsh
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+read -n1 -p "Add .zshrc? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+fi
+echo
 
 # tmux
-ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+read -n1 -p "Add .tmux.conf? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+fi
+echo
 
 # vim/neovim
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
-rm -rf ~/.dotfiles/.config/nvim/nvim
+read -n1 -p "Add .vimrc and .config/nvim? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+    ln -sf ~/.dotfiles/.config/nvim ~/.config/nvim
+    rm -rf ~/.dotfiles/.config/nvim/nvim
+fi
+echo
 
 # LaTeX
-ln -sf ~/.dotfiles/.latexmkrc ~/.latexmkrc
+read -n1 -p "Add .latexmkrc? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    ln -sf ~/.dotfiles/.latexmkrc ~/.latexmkrc
+fi
+echo
 
+# ideavim
+read -n1 -p "Add .ideavimrc? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    ln -sf ~/.dotfiles/.ideavimrc ~/.ideavimrc
+fi
+echo
 
-if ["$(uname)" == 'Darwin']; then
-    echo "Finish"
+# OS settings
+if [[ "$(uname)" == 'Darwin' ]]; then
+    echo "Your platform is Darwin. Finish."
     exit 1
 
 elif ["$(uname)" == 'Linux']; then
+    echo "Your platform is Linux"
     echo "Setup i3-wm and ranger config file."
     # i3
-    ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
-    rm -rf ~/.dotfiles/.config/i3/i3
+    read -n1 -p "Add .config/i3? (y/N): " yn
+    if [[ $yn = [yY] ]]; then
+        ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
+        rm -rf ~/.dotfiles/.config/i3/i3
+    fi
+    echo
 
-    ln -sf ~/.dotfiles/.Xresources ~/.Xresources
-    ln -sf ~/.dotfiles/.xprofile ~/.xprofile
-
+    # .Xresources
+    read -n1 -p "Add .Xresources? (y/N): " yn
+    if [[ $yn = [yY] ]]; then
+        ln -sf ~/.dotfiles/.Xresources ~/.Xresources
+    fi
+    echo
+    
+    # .xprofile
+    read -n1 -p "Add .xprofile? (y/N): " yn
+    if [[ $yn = [yY] ]]; then
+        ln -sf ~/.dotfiles/.xprofile ~/.xprofile
+    fi
+    echo
+    
     # ranger
-    ln -sf ~/.dotfiles/.config/ranger ~/.config/ranger
-    rm -rf ~/.dotfiles/.config/ranger/ranger
+    read -n1 -p "Add .config/ranger? (y/N): " yn
+    if [[ $yn = [yY] ]]; then
+        ln -sf ~/.dotfiles/.config/ranger ~/.config/ranger
+        rm -rf ~/.dotfiles/.config/ranger/ranger
+    fi
+    echo
+    
     exit 1
 
 else
